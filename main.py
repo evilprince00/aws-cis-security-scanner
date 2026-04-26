83ceafd7d2a9b66ebfd54820efc9e466a7d17d86
pratham kt <khatavakar1301050136@gmail.com>
2026-04-26 13:27:02 +0530

    Update main.py
    added banner for style

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from core.aws_session import AWSSessionManager
 from engine import ScanEngine
 from core.report import export_json_report
+from pyfiglet import Figlet
+from colorama import Fore, Style
 
 
 ROLE_ARN = "arn:aws:iam::984606368270:role/CisScannerRole"
 
 aws = AWSSessionManager(ROLE_ARN)
 aws.assume_role()
+
+def show_banner():
+    banner = Figlet(font='slant')
+    print(Fore.CYAN + banner.renderText('AWS CSPM SCANNER'))
+    print(Fore.YELLOW + "| Version = 1.1.0 | Author = evilprince00 |\n" + Style.RESET_ALL)
+
+show_banner()
 
 engine = ScanEngine(aws)
 results = engine.run_all()
